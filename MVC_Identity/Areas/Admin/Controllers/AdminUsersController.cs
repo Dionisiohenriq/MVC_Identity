fe0deb1c29b0b21d5656c44184711f3bc26fcd3c
Dionisiohenriq <henriquedionisio.dionisioferre@gmail.com>
2024-05-05 16:26:18 -0300

    Administração de users e roles

---
+++ MVC_Identity/Areas/Admin/Controllers/AdminUsersController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Identity.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminUsersController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdminUsersController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IQueryable<IdentityUser> users = _userManager.Users;
+            return View(users);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            IdentityUser? user = await _userManager.FindByIdAsync(id);
+
+            if (user is null)
+            {
+                ViewBag.ErrorMessage = $"Usuário com Id = {id} não foi encontrado.";
+                return View("NotFound");
+            }
+            else
+            {
+                IdentityResult result = await _userManager.DeleteAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View("Index");
+            }
+        }
+    }
+}
